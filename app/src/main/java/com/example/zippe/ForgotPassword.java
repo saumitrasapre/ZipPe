3cf82ba85f8dec81147cfc03d6f28b6458ece4e5
Saumitra Sapre <saumitrasapre69@gmail.com>
2020-06-09 22:51:13 +0530

    Added Forgot Password Module

---
+++ app/src/main/java/com/example/zippe/ForgotPassword.java
@@ -0,0 +1,70 @@
+package com.example.zippe;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    private EditText forgotpass_email;
+    private Button forgotpass_button;
+    private FirebaseAuth mAuth;
+    private ProgressDialog pd;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        forgotpass_email=findViewById(R.id.forgotpass_email);
+        forgotpass_button=findViewById(R.id.forgotpass_btn);
+
+        pd=new ProgressDialog(this);
+        pd.setMessage("Loading...");
+        pd.setCancelable(true);
+        pd.setCanceledOnTouchOutside(false);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        forgotpass_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email=forgotpass_email.getText().toString();
+
+                pd.show();
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(getApplicationContext(),"We have sent you instructions to reset your password...",Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(intent);
+                            pd.dismiss();
+                            finish();
+
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(),"Failed to send the password reset email",Toast.LENGTH_SHORT).show();
+                            pd.dismiss();
+                        }
+
+                    }
+                });
+            }
+
+            });
+        }
+    }
