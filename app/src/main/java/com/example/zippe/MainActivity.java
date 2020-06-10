dab2f14a2f4b3d517823a3420204d93afe0c8e00
Saumitra Sapre <saumitrasapre69@gmail.com>
2020-06-11 01:26:53 +0530

    facebook sign out added

--- app/src/main/java/com/example/zippe/MainActivity.java
+++ app/src/main/java/com/example/zippe/MainActivity.java
@@ -22,6 +22,7 @@ import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
